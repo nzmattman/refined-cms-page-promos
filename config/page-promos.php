af7cf1aba53a6ae95d33e3460bc889b9e720dee0
Matthias Law <matthias@refineddigital.co.nz>
2018-08-21 01:01:29 +0000

    initial commit

---
+++ config/page-promos.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'home' => [
+        'active' => true,
+    ],
+    'internal' => [
+        'active' => false,
+    ],
+    'fields' => [
+        'image' => [
+            'home' => [
+                'width' => 1920,
+                'height' => 600,
+            ],
+            'internal' => [
+                'width' => 1200,
+                'height' => 200
+            ]
+        ]
+    ]
+];
---
+++ src/Module/Providers/PagePromosServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RefinedDigital\PagePromos\Module\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use RefinedDigital\CMS\Modules\Core\Models\PageAggregate;
+
+class PagePromoserviceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../../config/page-promos.php' => config_path('page-promos.php'),
+        ], 'page-promos');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->mergeConfigFrom(__DIR__.'/../../../config/page-promos.php', 'page-promos');
+
+        $fields = [
+            [ 'name' => 'Heading', 'page_content_type_id' => 3, 'field' => 'heading' ],
+            [ 'name' => 'Link', 'page_content_type_id' => 3, 'field' => 'link' ],
+            [ 'name' => 'Image', 'page_content_type_id' => 4, 'field' => 'image', 'hide_label' => true ]
+        ];
+
+        if (config('page-promos.extra_fields')) {
+            $fields = array_merge($fields, config('page-promos.extra_fields'));
+        }
+
+        app(PageAggregate::class)
+            ->addModule('Promos', [
+                'tab' => 'promos',
+                'type' => 'repeatable',
+                'config' => 'page-promos',
+                'fields' => $fields
+            ]);
+    }
+}
